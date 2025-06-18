9ff38b89bd23bd89b2d69c3af4f73a9744c79b30
AkashP20031 <akashp@ivoyant.com>
2025-06-18 15:29:15 +0530

    java

---
+++ src/ArraysExample/Array2D.java
@@ -0,0 +1,23 @@
+package ArraysExample;
+
+public class Array2D {
+    public static void main(String[] args) {
+        int[][] a = {{2,3,5,8},{3,6,5,7}};
+        for(int i = 0; i<a.length ;i++)
+        {
+            for (int j = 0; j<a[i].length;j++)
+            {
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int[] x : a) {
+            for (int y : x) {
+                System.out.print(y);
+            }
+            System.out.println();
+        }
+    }
+}
--- src/ArraysExample/Challenge4.java
+++ src/ArraysExample/Challenge4.java
@@ -2,7 +2,7 @@ package ArraysExample;
 
 public class Challenge4 {
     public static void main(String[] args) {
-        int a[] = {1,2,3,4,15,6,7,8};
+        int[] a = {1,2,3,4,15,6,7,8};
 
         for (int i=a.length - 1 ;i>=0; i-- )
         {

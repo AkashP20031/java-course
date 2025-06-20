749429d9bc42b4e388743518995b75ee7b3be4fe
AkashP20031 <akashp@ivoyant.com>
2025-06-20 10:58:30 +0530

    java

---
+++ src/StaticAndFinal/FinalKeyword.java
@@ -0,0 +1,8 @@
+package StaticAndFinal;
+
+public class FinalKeyword {
+    static final float PI  = 3.14f;
+    public static void main(String[] args) {
+        System.out.println(" final keyword PI value : " +PI);
+    }
+}
